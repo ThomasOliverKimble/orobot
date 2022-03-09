be1b380e82301a5b5d58f5713c98332e3f7b2d3a
ThomasOliverKimble <tokimble@gmail.com>
2022-03-09 16:09:59 +0100

    Orobot Dark added

---
+++ plugins/robot_windows/orobotDark/orobotDark.js
@@ -0,0 +1,12 @@
+import RobotWindow from 'https://cyberbotics.com/wwi/R2022b/RobotWindow.js';
+
+window.onload = function() {
+  window.robotWindow = new RobotWindow();
+  window.robotWindow.setTitle('Orobot Robot Dark Window');
+  window.robotWindow.send("Robot window loaded");
+};
+
+window.updateValue  = function(name, value) {
+  document.getElementById('parameter_' + name + '_display').innerHTML = value;
+  window.robotWindow.send("parameter: " + name + "=" + value);
+}
